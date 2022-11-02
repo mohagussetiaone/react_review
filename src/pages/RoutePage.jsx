c18be5b654eb53a4d249cac3342ec485376fa26a
mohagussetiaone <mohagussetiaone@gmail.com>
2022-11-02 13:49:25 +0700

    update Route Page

--- src/pages/RoutePage.jsx
+++ src/pages/RoutePage.jsx
@@ -2,20 +2,15 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function RoutePage() {
-  // state
-  const [post, setPost] = useState([]);
+  //state
+  const [post, setPost] = useState();
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   // function
   const getPost = async () => {
     try {
-      const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
       const data = await result.json();
       return data;
     } catch (error) {
@@ -23,32 +18,26 @@ export default function RoutePage() {
     }
   };
 
-  // comp life cycle
+  // clc
   useEffect(() => {
     getPost()
       .then((res) => {
-        console.info(res);
         setPost(res);
         setLoading(false);
       })
       .catch((err) => {
         console.error(err);
       });
-  });
+  }, []);
 
-  // validation
   if (loading) {
-    return <div>Loading Please Wait</div>;
+    return <div>loading please wait...</div>;
   }
+
   return (
     <div className="App">
-      <h1>Detail Page</h1>
-      {post.map((e) => (
-        <div key={e.id}>
-          <h1>{e.title}</h1>
-          <p>{e.body}</p>
-        </div>
-      ))}
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 }
